09af52de346ea80642392906e05cfd5d4397abb8
cruelw1nter <99076325+cruelw1nter@users.noreply.github.com>
2022-09-19 16:07:17 +0300

    refr cv

--- new-cv/js/script.js
+++ new-cv/js/script.js
@@ -7,9 +7,9 @@ $(document).ready(function() {
     });
 });
 
-//home
+//homw
 $(window).scroll(function() {
-    $('.home').toggleClass('home--show', $(this).scrollTop() > 50);
+    $('home').toggleClass('home--show', $(this).scrollTop() > 50);
 });
 
 //тень на скролле
